4fd7bac9899b819949604f7e8aefb5f52177aa1b
Truong Hoang Kha <kha@gmail.com>
2023-11-12 13:47:59 +0700

    Update component calendar

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -9,6 +9,125 @@ const Calendar = () => {
           <div class="container">
             <div class="text-center">
               <h1 class="display-4">Schedule</h1>
+              <div class="d-flex justify-content-end gap-3 text-right -mt-14">
+                <div>
+                  <input type="date" name="" id="" />
+                </div>
+                <div>
+                  <button
+                    class="btn btn-success p-3"
+                    data-toggle="modal"
+                    data-target="#exampleModal"
+                  >
+                    Confirm
+                  </button>
+                </div>
+                <div
+                  class="modal fade"
+                  id="exampleModal"
+                  tabindex="-1"
+                  role="dialog"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                          Appoinment
+                        </h5>
+                      </div>
+                      <div class="modal-body" style="text-align : left">
+                        <div class="mb-3">
+                          <h4>Patient Information</h4>
+                          <div class="mb-3">
+                            <div class="row">
+                              <div class="col-6">
+                                <p>
+                                  <strong>First Name : </strong>
+                                  <span>Van</span>
+                                </p>
+                              </div>
+                              <div class="col-6">
+                                <p>
+                                  <strong>Last Name : </strong>
+                                  <span>Nguyen</span>
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="mb-3">
+                            <p>
+                              <strong>Phone : </strong>
+                              <span>0901233312</span>
+                            </p>
+                          </div>
+                          <div class="mb-3">
+                            <p>
+                              <strong>Date of birth : </strong>
+                              <span>12/12/2003</span>
+                            </p>
+                          </div>
+                          <div class="mb-3">
+                            <p>
+                              <strong>Gender : </strong>
+                              <span>Nam</span>
+                            </p>
+                          </div>
+                          <div class="mb-3">
+                            <p>
+                              <strong>Address : </strong>
+                              <span>123ABC, Street 12</span>
+                            </p>
+                          </div>
+                        </div>
+                        <div class="mb-3">
+                          <h4>Dentist Information</h4>
+                          <div class="mb-3">
+                            <p>
+                              <strong>Name : </strong>
+                              <span>Ts. Nguyen Van A</span>
+                            </p>
+                          </div>
+                          <div class="mb-3">
+                            <p>
+                              <strong>Phone : </strong>
+                              <span>0901233312</span>
+                            </p>
+                          </div>
+                          <div class="mb-3">
+                            <p>
+                              <strong>Gender : </strong>
+                              <span>Nam</span>
+                            </p>
+                          </div>
+                        </div>
+                        <div class="mb-3">
+                          <h4>Appointment</h4>
+                          <div class="mb-3">
+                            <p>
+                              <strong>Date: </strong>
+                              <span>12/12/2009</span>
+                            </p>
+                          </div>
+                          <div class="mb-3">
+                            <strong class="d-inline-block">Shift : </strong>
+                            <p class="d-inline-block">
+                              Morning <span>8h30</span>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-primary">
+                          Confirm
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <p class="lead">
                 Choose your dentist and then choose your date or vice versa
               </p>
--- components/dateCalendarItem.tsx
+++ components/dateCalendarItem.tsx
@@ -3,6 +3,7 @@ import * as elements from "typed-html";
 const DateCalendarItem = () => {
   return (
     <div class="col-xs-12 calendar-day">
+      <div>Ths. Nguyen Van A</div>
       <time datetime="2014-07-07">07</time>
     </div>
   );
